28d5a09c6eba382c105a643ba4ff197e65ca3954
Yashan Thilakasiri <52060474+YashanThilakasiriMMYS@users.noreply.github.com>
2020-07-14 02:02:09 +0530

    Business card template creating started. added the profile pic and name

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,37 @@
+import 'package:flutter/material.dart';
 
+//--------------------IMPORTANT---------------------
+//all the thigs are also can directly apply to the Row
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: AssetImage('images/me.jpg'),
+              ),
+              Text(
+                'YashanMick',
+                style: TextStyle(
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
